8a5486f7f7d318d7cd974c5b2064026283cad5f9
Slainez <sullyvan360@gmail.com>
2022-08-17 10:12:43 +0200

    ajout persistence xml

--- src/main/java/fr/m2i/jpa/hibernate/JpaHibernate.java
+++ src/main/java/fr/m2i/jpa/hibernate/JpaHibernate.java
@@ -1,17 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package fr.m2i.jpa.hibernate;
 
-/**
- *
- * @author moi
- */
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
 public class JpaHibernate {
 
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("site_commerce_pu");
+        EntityManager entityManager = emf.createEntityManager();
         System.out.println("Hello World!");
     }
 }
